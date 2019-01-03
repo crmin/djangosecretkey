7c38ea4c73244d53cdd0a1fe9ad987f8a9f16f8a
crmin <blinglnav@gmail.com>
2019-01-03 17:25:30 +0900

    Delete tmp test file after test done

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -6,6 +6,20 @@ from secret_key import secret_key
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+SECRET_KEY_PATHS = [
+    './dir/.secret_key',  # Check relative path
+    './.secret_key',  # Check relative path but, base dir == '.'
+    '.secret_key_2',  # Check relative path but, base dir == ''
+    os.path.join(BASE_DIR, '.secret_key_3'),  # Check abs path
+    './.secret_key',  # Check file already exist
+]
+
+
+def teardown_module(module):
+    for path in set(SECRET_KEY_PATHS):
+        os.remove(path)
+    os.removedirs('./dir')
+
 
 def test_generate():
     assert len(secret_key.generate()) == 50
@@ -13,13 +27,7 @@ def test_generate():
     assert len(secret_key.generate(70)) == 70
 
 
-@pytest.mark.parametrize("path", [
-    './dir/.secret_key',  # Check relative path
-    './.secret_key',  # Check relative path but, base dir == '.'
-    '.secret_key_2',  # Check relative path but, base dir == ''
-    os.path.join(BASE_DIR, '.secret_key_3'),  # Check abs path
-    './.secret_key',  # Check file already exist
-])
+@pytest.mark.parametrize("path", SECRET_KEY_PATHS)
 def test_from_file(path):
     key = secret_key.from_file(path)
     with open(path, 'r') as f:
